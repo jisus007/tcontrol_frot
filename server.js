7f971b1feee43811f136b3ef3d6025cc4f0cdd5f
camachoarguijo634@gmail.com <camachoarguijo634@gmail.com>
2018-07-26 13:03:28 -0500

    updates to deploy

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/tcontrol-cliente'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/app.component.html'));
+app.get('/*all', function(req,res) {
+        res.sendFile(path.join(__dirname+'/app.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
