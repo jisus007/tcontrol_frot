757733d61feebc6166337675e5a107f75321a2cd
camachoarguijo634@gmail.com <camachoarguijo634@gmail.com>
2018-07-26 12:00:09 -0500

    updates to deploy to heroku 8

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tcontrol_frot'));
+app.use(express.static(__dirname + '/tcontrol-cliente'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tcontrol_frot/index.html'));
+res.sendFile(path.join(__dirname+'/tcontrol-cliente/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
