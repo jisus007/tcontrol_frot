fb44b9c42357de619247a1fafdb7d63f33a90bd1
camachoarguijo634@gmail.com <camachoarguijo634@gmail.com>
2018-07-26 11:34:12 -0500

    updates to deploy to heroku 5

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tcontrol-cliente'));
+app.use(express.static(__dirname + '/dist/app-rastreo-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tcontrol-cliente'));
+res.sendFile(path.join(__dirname+'/dist/app-rastreo-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/_services/objeto.service.ts
+++ src/app/_services/objeto.service.ts
@@ -9,11 +9,14 @@ import { Observable } from '../../../node_modules/rxjs';
 })
 export class ObjetoService {
 
+  //creamoms variable API, es la direccion donde consumiremos nuestro servicios.
   public API = 'http://localhost:5000';
   //public DIR_API = this.API + '/apicontroller';
 
+  //Agregamos al constructor http de tipo HttpClient
   constructor(private http:HttpClient) { }
 
+  //Creamos el metodo obtenerTodo, este metodo obtiene todos los objetos
   obtenerTodo() {
     return this.http.get(this.API + '/objetos/');
   }
