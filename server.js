ae446c82693ed0a9145ee845400cdf6f9e8348c6
camachoarguijo634@gmail.com <camachoarguijo634@gmail.com>
2018-07-26 13:09:01 -0500

    updates to deploy to heroku 8

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/tcontrol-cliente'));
 
 app.get('/*all', function(req,res) {
-        res.sendFile(path.join(__dirname+'/app.component.html'));
+    
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
