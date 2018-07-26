9b17716d0c9e36ba16186ea56db915e0993ca2d3
camachoarguijo634@gmail.com <camachoarguijo634@gmail.com>
2018-07-26 11:40:14 -0500

    updates to deploy to heroku 7

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app-rastreo-web'));
+app.use(express.static(__dirname + '/dist/tcontrol_frot'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/app-rastreo-web/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tcontrol_frot/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
